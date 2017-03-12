fd3c76568e5293d2d73c54103aa1c4b764ebab0a
nonis3 <nonis@purdue.edu>
2017-03-12 20:05:29 +0000

    Base scraping logic for 3 endpoints

--- scrape.rb
+++ scrape.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'nokogiri'
 require 'open-uri'
+require 'json'
 
 contests = [ ]
+
+HACKERRANK_URL = "https://www.hackerrank.com/rest/contests"
+COOKIE = "not commiting this lol"
 
 (1..9).each do |i|
   contests << "purdue-competitive-programming-week-#{i}"
@@ -14,11 +17,27 @@ end
   contests << "purdue-advanced-competitive-programming-week-#{i}"
 end
 
-def get(i)
-  url = "https://www.hackerrank.com/contests/#{i}/challenges"
-  puts url
-  page = Nokogiri::HTML(open(url))   
-  puts page.text
+def get_json(url)
+  page = open(url, 'Cookie' => COOKIE)
+  json = JSON.parse(page.read)
+  return json['models']
 end
 
-get contests[0]
+def get_leaderboard(contest_name, offset = 0, limit = 30)
+  url = "#{HACKERRANK_URL}/#{contest_name}/leaderboard?offset=#{offset}&limit=#{limit}"
+  get_json(url)
+end
+
+def get_challenges(contest_name, offset = 0, limit = 30)
+  url = "#{HACKERRANK_URL}/#{contest_name}/challenges?offset=#{offset}&limit=#{limit}"
+  get_json(url)
+end
+
+def get_challenge_leaderboard(contest_name, challenge_name, offset = 0, limit = 30)
+  url = "#{HACKERRANK_URL}/#{contest_name}/challenges/#{challenge_name}/leaderboard?offset=#{offset}&limit=#{limit}"
+  get_json(url)
+end
+
+
+scores = get_leaderboard 'purdue-advanced-competitive-programming-week-5'
+puts scores
